8c70b1f431c3f4272faee4c1ca48a78542c8b295
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-05-15 12:39:48 -0400

    Made dropdown on click

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-console.log("test")
+const dropDownButton = document.querySelector("#dropDownShowCLick")
+const dropDownMenu = document.querySelector("#dropDownMenu")
+const dropDownMenuButtons = document.querySelectorAll(".selectOption")
+
+dropDownMenuButtons.forEach((button) =>{
+    button.addEventListener("click" , () =>{
+        console.log(button.id)
+    })
+})
+
+
+
+dropDownButton.addEventListener("click", () => {
+  
+    console.log(dropDownMenu.style.opacity)
+
+    if(dropDownMenu.style.opacity == 0){
+        dropDownMenu.style.opacity = 100
+    }else{
+        dropDownMenu.style.opacity = 0
+    }
+  });
