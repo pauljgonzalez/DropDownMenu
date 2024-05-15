8e21324fba63ba5a43cb05b1965df4dc9b190091
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-05-15 14:12:26 -0400

    Added hover button and changed logic

--- scripts.js
+++ scripts.js
@@ -1,22 +1,35 @@
-const dropDownButton = document.querySelector("#dropDownShowCLick")
-const dropDownMenu = document.querySelector("#dropDownMenu")
-const dropDownMenuButtons = document.querySelectorAll(".selectOption")
+const toggleDropDown = document.querySelectorAll(".dropDownShow")
+const optionButtons = document.querySelectorAll(".selectOption")
+toggleDropDown.forEach((button) =>{
+    if(button.parentNode.childNodes[1].id == "dropDownShowCLick"){
+        button.addEventListener("click", () =>{
+                    let opacity = button.parentNode.parentNode.lastChild.previousElementSibling.style.opacity 
+                    if(opacity  == "" || opacity == 0){
+                        button.parentNode.parentNode.lastChild.previousElementSibling.style.opacity  = 100
+                        // button.parentNode.parentNode.lastChild.previousElementSibling.style.cursor = "pointer"
+                    } else{
+                        button.parentNode.parentNode.lastChild.previousElementSibling.style.opacity  = 0
+                        
+                    }
+            })
+        }  
+        if(button.parentNode.childNodes[1].id == "dropDownShowHover"){
+            button.addEventListener("mouseover", () =>{
+                    let opacity = button.parentNode.parentNode.lastChild.previousElementSibling.style.opacity 
+                    if(opacity  == "" || opacity == 0){
+                        button.parentNode.parentNode.lastChild.previousElementSibling.style.opacity  = 100
+                    } else{
+                        button.parentNode.parentNode.lastChild.previousElementSibling.style.opacity  = 0 
+                }
+            })
+            button.parentNode.parentNode.addEventListener("mouseleave", () =>{
+                    button.parentNode.parentNode.lastChild.previousElementSibling.style.opacity  = 0 
+        })
+        }
+})
 
-dropDownMenuButtons.forEach((button) =>{
-    button.addEventListener("click" , () =>{
+optionButtons.forEach((button) =>{
+    button.addEventListener("click",()=>{
         console.log(button.id)
     })
 })
-
-
-
-dropDownButton.addEventListener("click", () => {
-  
-    console.log(dropDownMenu.style.opacity)
-
-    if(dropDownMenu.style.opacity == 0){
-        dropDownMenu.style.opacity = 100
-    }else{
-        dropDownMenu.style.opacity = 0
-    }
-  });
